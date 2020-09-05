a0427904443bedf6b6f0d3de846771aa8ee8624e
PoixoN <poixon2001@gmail.com>
2020-09-05 20:14:37 +0300

    New functions

--- Mini game - Hungry Turtle/Program.cs
+++ Mini game - Hungry Turtle/Program.cs
@@ -9,23 +9,40 @@ namespace Mini_game___Hungry_Turtle
 {
     class Program
     {
+        static void WriteT(int size)
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            Turtle.Angle = 270;
+            Turtle.Move(size / 3);
+            Turtle.Angle = 90;
+            Turtle.Move(size * 2 / 3 );
+        }
+        static void WriteO(int size)
+        {
+            Turtle.Angle = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.Move(size);
+                Turtle.TurnRight();
+            }
+        }
         static void Main(string[] args)
         {
-            Turtle.Speed = 9;
+            Turtle.Speed = 8;
 
-            int cnt = 0;
-            while (cnt < 10)
-            {
-                Turtle.Move(25);
-                Turtle.TurnRight();
-                Turtle.Move(25);
-                Turtle.TurnRight();
-                Turtle.Move(25);
-                Turtle.TurnLeft();
-                Turtle.Move(25);
-                Turtle.TurnLeft();
-                ++cnt;
-            }
+            Turtle.X = 200;
+            Turtle.Y = 200;
+            WriteT(60);
+
+            Turtle.X = 260;
+            Turtle.Y = 200;
+            WriteO(30);
+
+            Turtle.X = 400;
+            Turtle.Y = 200;
+            WriteT(30);
+
         }
     }
 }
