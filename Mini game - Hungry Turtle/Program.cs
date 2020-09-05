2e7868ac01196176ba685de192e8dba40ba0c768
PoixoN <poixon2001@gmail.com>
2020-09-05 19:56:32 +0300

    Добавьте файлы проекта.

---
+++ Mini game - Hungry Turtle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace Mini_game___Hungry_Turtle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 9;
+
+            int cnt = 0;
+            while (cnt < 10)
+            {
+                Turtle.Move(25);
+                Turtle.TurnRight();
+                Turtle.Move(25);
+                Turtle.TurnRight();
+                Turtle.Move(25);
+                Turtle.TurnLeft();
+                Turtle.Move(25);
+                Turtle.TurnLeft();
+                ++cnt;
+            }
+        }
+    }
+}
